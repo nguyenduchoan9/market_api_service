9a8cba29a07dbcd88bfc1524821469cf2b8d048e
nguyendhoang <hoang.nguyen@saleshood.com>
2017-04-16 23:53:25 +0700

    Adds update action the users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 end
 # prax https://github.com/ysbaddaden/prax/issues/87
+# sudo iptables -t nat -L -n
