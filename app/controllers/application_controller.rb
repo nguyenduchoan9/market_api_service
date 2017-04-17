d1e33353ca9bce43ecf723858b100dd0d7919e84
nguyendhoang <hoang.nguyen@saleshood.com>
2017-04-17 08:59:37 +0700

    Fix error api url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,3 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 end
 # prax https://github.com/ysbaddaden/prax/issues/87
-# sudo iptables -t nat -L -n
