9be65cb3fea6a1f98b4006bafbe3e55ee8297e26
nguyendhoang <hoang.nguyen@saleshood.com>
2017-04-20 22:40:38 +0700

    message

--- config/application.rb
+++ config/application.rb
@@ -35,10 +35,11 @@ module MarketPlaceApi
     config.autoload_paths += %W(\#{config.root}/lib)
   end
 
-  if defined?(Bundler)
-    # If you precompile assets before deploying to production, use this line
-    # Bundler.require(*Rails.groups(:assets => %w(development test)))
-    # If you want your assets lazily compiled in production, use this line
-    Bundler.require(:default, :assets, Rails.env)
-  end
+
+end
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  Bundler.require(:default, :assets, Rails.env)
 end
